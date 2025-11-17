e57d7fb3c4e280dfc1e1d2781bf41bd7a224a060
sahaj-b <sahajb0606@gmail.com>
2025-11-17 18:15:41 +0530

    add timeout option -t

--- default-config.sh
+++ default-config.sh
@@ -3,6 +3,7 @@ FOREGROUND=false # Run timers in foreground by default
 NO_SOUND=false   # Disable sound by default
 SOUND_FILE=""    # Path to custom sound file, eg: "/path/to/sound.wav"
 VOLUME=""        # Default volume
+TIMEOUT=0        # Notification/sound timeout in seconds, 0 means no timeout
 
 STATE_FILE="$HOME/.cache/tymr_state" # File to store timers' state
 DELIMITER="|"                        # Delimiter for separating fields in the state file, must not be in timer names
